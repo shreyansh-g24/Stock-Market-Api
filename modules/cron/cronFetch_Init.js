55f674b26df460bd7c370cf5790619ff5aa2d336
SG24 <shreyanshgupta98@gmail.com>
2019-06-10 16:40:48 +0530

    added modules, integrated cron, and normalising api data

---
+++ modules/cron/cronFetch_Init.js
@@ -0,0 +1,139 @@
+// MODULES: CRON_FETCH.JS //
+// Fetching Data and updating DB using cron //
+
+// Importing modules //
+let fetch = require("node-fetch");
+let cron = require("node-cron");
+let HomeData = require("./../../models/HomeData");
+
+// Declaring Global Variables and constants //
+const FM_CONFIG = require("./../financialModellingApi/fm_config");
+let _gTempStore = {};
+let _gDataStore = {};
+
+// Declaring functions //
+
+// updating Home data document, or creating one if none exists
+// data: receives data to be updated
+function updateHomeData(data = null) {
+
+  if (!data) return consoleError("updateHomeData called with insufficient arguments!");
+
+  HomeData.find({}, (err, docs) => {
+    if (err) return consoleError(JSON.stringify(err));
+    // if no doc exists
+    if (docs.length === 0) {
+      HomeData.create({ data: JSON.stringify(data) }, (err, doc) => {
+        if (err) return consoleError(JSON.stringify(err));
+        return doc;
+      });
+      return 0;
+    }
+    // if docs exist
+    let _id = docs[0]._id;
+    HomeData.findByIdAndUpdate(_id, { data: JSON.stringify(data) }, { new: true, useFindAndModify: false }, (err, doc) => {
+      if (err) return consoleError(JSON.stringify(err));
+      return doc;
+    });
+  });
+  return 0;
+}
+
+// Resetting _gDataStore value
+// Returns 0 on success
+function reset_gStore() {
+  _gTempStore = {};
+  _gTempStore = Object.assign({}, _gDataStore);
+  _gDataStore = {};
+  return 0;
+}
+
+// Printing error in console
+function consoleError(err = null) {
+  if (!err) return 1;
+  console.log(__filename + ": err: ", err);
+  return 0;
+}
+
+// fetching and updating global data store
+// apiUrls: object with key value pairs being headers and urls to fetch the data from.
+// updates _gDataStore variable with the fetched data
+// returns 0 on success
+function fetchData(apiUrls = null) {
+
+  reset_gStore();
+
+  if (apiUrls === null) {
+    consoleError("fetchData called with insufficient arguments!");
+    return 1;
+  }
+
+  for (let key in apiUrls) {
+
+    fetch(apiUrls[key])
+      .then(response => response.json())
+      .then(data => _gDataStore[key] = data)
+      .catch(err => {
+
+        _gDataStore[key] = { errMessage: "Unable to fetch data!", err };
+
+      });
+  }
+
+  return 0;
+}
+
+// cron job to fetch the data
+// UPDATE fetchTime here to change fetching frequency
+let task_fetchData = (function () {
+  let fetchTime = "*/2 * * * *";
+
+  let task = cron.schedule(fetchTime, () => {
+    fetchData(FM_CONFIG.apiURLs);
+  });
+
+  return task;
+})();
+
+// Declaring and exporting init function //
+// UPDATE taskTime here to change the frequency of checking if the market is open.
+let cronInit = function () {
+  // Declaring time variables
+  let taskTime = "* * * * *";
+
+  // Creating cron task
+  let task = cron.schedule(taskTime, () => {
+
+    // updating home data, irrespective of whether the market is open or not.
+    updateHomeData(FM_CONFIG.normalizeHomeData(_gDataStore));
+
+    // Initial fetch if _gDataStore is empty
+    if (Object.entries(_gDataStore).length === 0 && _gDataStore.constructor === Object) {
+      fetchData(FM_CONFIG.apiURLs);
+    }
+
+    // Checking if market is open
+    FM_CONFIG.isOpenNYSE()
+      .then(isOpen => {
+
+        // If market is open, start fetching task
+        if (isOpen) {
+          task_fetchData.start();
+        }
+        // if market is closed, stop fetching task
+        else if (!isOpen) {
+          task_fetchData.stop();
+        }
+
+      })
+      .catch(err => {
+        consoleError("isOpenNYSE emitted unexpected error: ", JSON.stringify(err));
+      });
+  });
+
+  task.start();
+
+  return 0;
+};
+
+module.exports = cronInit;
---
+++ modules/financialModellingApi/fm_config.js
@@ -0,0 +1,179 @@
+// FINANCIAL MODELLING API (VERSION 3) : CONFIG //
+// Importing Modules
+let fetch = require("node-fetch");
+
+module.exports = {
+
+  // Declaring global variables and constants //
+  constants: {
+    // NY Time zone offset
+    TimezoneOffset_NY: -4,
+    // NYSE timings
+    NYSE_open_hours: 9,
+    NYSE_open_minutes: 30,
+    NYSE_close_hours: 16,
+    NYSE_close_minutes: 0,
+  },
+
+  // URL's for financial modelling api calls
+  apiURLs: {
+
+    // is the market open, holidays, exchange's name, working hours
+    "NYSE Hours": "https://financialmodelingprep.com/api/v3/is-the-market-open",
+
+    // Daily most active, loser and daily stocks
+    "Most Active Stocks (Daily)": "https://financialmodelingprep.com/api/v3/stock/actives",
+    "Most Gainer Stocks (Daily)": "https://financialmodelingprep.com/api/v3/stock/gainers",
+    "Most Loser Stocks (Daily)": "https://financialmodelingprep.com/api/v3/stock/losers",
+
+    // Sectors' performances
+    "Sectors": "https://financialmodelingprep.com/api/v3/stock/sectors-performance",
+
+    // Major Indexes Realtime Performance
+    "Major Indexes": "https://financialmodelingprep.com/api/v3/majors-indexes",
+
+    // Forex Realtime OHLC
+    "Forex": "https://financialmodelingprep.com/api/v3/forex",
+  },
+
+  // printing error in console
+  consoleError: function (err) {
+    console.log(__filename + ": err: " + err);
+    return 0;
+  },
+
+  // Checking if the stock market is open
+  isOpenNYSE: async function () {
+
+    // Getting new york time
+    let nytime = this.calcTime(this.constants.TimezoneOffset_NY);
+    let nytime_hours = nytime.getHours();
+    let nytime_minutes = nytime.getMinutes();
+
+    // if timenow is between nyse open hours
+    if (nytime_hours >= this.constants.NYSE_open_hours &&
+      nytime_minutes >= this.constants.NYSE_open_minutes &&
+      nytime_hours <= this.constants.NYSE_close_hours &&
+      nytime_minutes <= this.constants.NYSE_close_minutes) {
+
+      // fetching and returning status
+      let response = await fetch(this.apiURLs["NYSE Hours"]);
+      let data = response.json();
+
+      return data.then(isOpen => isOpen.isTheStockMarketOpen);
+    }
+    // if timenow doesn't lie between nyse open hours
+    else {
+      return false;
+    };
+  },
+
+  // Calculating time given the timezone offset of target place
+  // Receives time zone offset e.g. ("-4" for new york | Eastern Daylight Time)
+  // Returns current new york time
+  calcTime: function (offset) {
+    let timeNow = new Date();
+    let utcTime = timeNow.getTime() + (timeNow.getTimezoneOffset() * 60000);
+    let targetCityTime = new Date(utcTime + (3600000 * offset));
+    return targetCityTime;
+  },
+
+  // Normalizing home page data: actives, losers, gainers, indexes, sectors, forex
+  // data: Receives data to be nomalised, an object with all the information above.
+  normalizeHomeData: function (dataObj = 0) {
+
+    // Declaring variables
+    let normalizedData = [];
+    let tabData = {};
+
+    // Handling errors
+    if (dataObj === 0) {
+      this.consoleError("normalizeHomeData called with insufficient arguments!");
+      return 1;
+    }
+    else if ((Object.entries(dataObj).length === 0 && dataObj.constructor === Object) || dataObj.constructor === Array) {
+      tabData = { err: "normalizeHomeData called with inaccurate arguments!" };
+      normalizedData.push(tabData);
+      return normalizedData;
+    }
+
+    // normalising data
+    for (let key in dataObj) {
+
+      // resetting tabData
+      tabData = {};
+
+      // if there's error or the data is for NYSE hours
+      if (dataObj[key].err || key === "NYSE Hours") {
+        tabData = dataObj[key];
+      }
+      // if there's no error
+      else if (!dataObj[key].err) {
+
+        // saving heading
+        tabData.head = key;
+
+        // accessing nested object
+        for (let key1 in dataObj[key]) {
+
+          // saving keyword
+          tabData.keyword = key1;
+
+          let _originalHeaders = [];
+          // checking if the data is an array
+          if(dataObj[key][key1].constructor === Array){
+            tabData.data = dataObj[key][key1].map(val => {
+              // looping through keys in val if not done already to save original headers
+              if (_originalHeaders.length === 0) {
+                for (let header in val) {
+                  _originalHeaders.push(header);
+                }
+                // adding original headers list to tabData
+                tabData._originalHeaders = _originalHeaders;
+              }
+
+              // Declaring variables to store val's values
+              let name;
+              let change;
+              let changePercentage;
+              let price;
+
+              // Handling name
+              if (val.ticker && val.companyName) name = val.companyName + " (" + val.ticker + ")";
+              else if (val.ticker && val.indexName) name = val.indexName + " (" + val.ticker + ")";
+              else if (val.ticker) name = val.ticker;
+              else if (val.sector) name = val.sector;
+              else name = "undefined";
+              // Handling changes and changesPercentage
+              val.changes ? change = val.changes : "undefined";
+              val.changesPercentage ? changePercentage = val.changesPercentage : "undefined";
+              // Handling Price
+              if (val.price) price = val.price;
+              else if (val.bid && val.ask && val.open && val.high && val.low) {
+                let { bid, ask, open, high, low } = val;
+                price = { bid, ask, open, high, low };
+              }
+              else price = "undefined";
+
+              // replacing "%5" in names with "."
+              if (typeof (name) === "string" && name.includes("%5")) name = name.replace("%5", ".");
+
+              // returning original data if no match found
+              if (name === "undefined" && change === "undefined" && changePercentage === "undefined" && price === "undefined") {
+                return val;
+              }
+              return { name, change, changePercentage, price };
+            });
+          }
+          else if(dataObj[key][key1].constructor !== Array){
+            tabData.data = dataObj[key][key1];
+          }
+        }
+      }
+      // pushing to normalize data array
+      normalizedData.push(tabData);
+    }
+    return normalizedData;
+  },
+
+};
