09b8b2dceeca61ccf6c21324aa5ba8b0dcee6079
SG24 <shreyanshgupta98@gmail.com>
2019-06-11 23:01:49 +0530

    implemented model and instance methods for jwt in user model

--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@
 
 // Importing Modules
 let mongoose = require("mongoose");
+let jwt = require("jsonwebtoken");
+
+// Importing config file
+const config = require("./../config/config");
 
 // Defining user schema
 let userSchema = new mongoose.Schema({
@@ -21,9 +25,36 @@ let userSchema = new mongoose.Schema({
     type: String,
     minlength: 6,
   },
+  access: {
+    type: String,
+    default: "authorized",
+  }
 });
 
-// Declaring model and instance methods
+// Declaring model and instance methods //
+// generating auth token
+userSchema.methods.generateAuthToken = function () {
+  let user = this;
+  let token = jwt.sign({ _id: user._id.toHexString(), access: user.access }, config.SECRET);
+  return token;
+}
+
+// finding user by token
+// token: receives a token
+// returns a promise, rejecting if error, user if success 
+userSchema.statics.findByToken = function (token) {
+  let User = this;
+  let decoded;
+
+  // trying to decode the token, returning a promise with rejection if error
+  try {
+    decoded = jwt.verify(token, config.SECRET);
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({_id: decoded._id});
+}
 
 // requiring model
 let UserModel = mongoose.model("User", userSchema);
