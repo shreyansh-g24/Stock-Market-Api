8aa69d0aed55ed4061c3ef0802e7e7534179115f
SG24 <shreyanshgupta98@gmail.com>
2019-06-14 13:09:34 +0530

    sets up cc api urls fetch route

--- api/v1/fm.js
+++ api/v1/fm.js
@@ -7,15 +7,19 @@ let router = express.Router();
 
 // requiring controller
 let fmCont = require("./fmController");
+let userCont = require("./../../controllers/userController");
 
 /**
  * Handled Routes:
  *  => /api/v1/fm
- *    => /home :- GET: Returning basic data: major indexes, gainer loser active stocks, forex, sectors, NYSE
- *    => /nyse :- GET: Returning nyse data only
+ *    => /home :- GET: Returns basic data: major indexes, gainer loser active stocks, forex, sectors, NYSE
+ *    => /nyse :- GET: Returns nyse data only
+ *    => /cc :- GET: Returns crypto currency data urls after authentication JWT 
  */
 router.get("/home", fmCont.returnHomeData);
 router.get("/nyse", fmCont.returnNYSEData);
+router.get("/cc", userCont.authenticateJWT, fmCont.returnCCURL);
+
 
 // Exporting router
 module.exports = router;
--- api/v1/fmController.js
+++ api/v1/fmController.js
@@ -1,9 +1,16 @@
 // FMCONTROLLER.JS //
 let HomeData = require("./../../models/HomeData");
+const FM_CONFIG = require("./../../modules/financialModellingApi/fm_config");
 
 module.exports = {
 
-  // sending home data
+  // returns crypto currency data url
+  returnCCURL: function(req, res, next){
+    if(!req.auth.success) res.json(req.auth);
+    else if(req.auth.success) res.json({success: true, ccApiUrl: FM_CONFIG.ccApiURLs});
+  },
+
+  // sends home data
   returnHomeData: function(req, res, next){
     HomeData.find({}, (err, docs) =>{
       if(err) return next(err);
@@ -14,14 +21,14 @@ module.exports = {
     });
   },
 
-  // returning nyse data
+  // returns nyse data
   returnNYSEData: function(req, res, next){
     HomeData.find({}, (err, docs) => {
       if(err) return next(err);
 
       if(docs.length === 0) return res.json({err: "Unable to find the requested Data!"});
 
-      // extracting and returning NYSE data
+      // extracts and returns NYSE data
       docs = JSON.parse(docs[0].data);
       let NYSEData = docs.filter((d) => {
         return d.stockExchangeName && !d.head;
